89ca9e26fc14390f01eb8508ab87e7ab4fd23348
uyttyu7532 <uyttyu7532@naver.com>
2020-08-20 04:22:56 +0900

    init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="azure-background">
+    <img src={logo} lat="logo"/>
+    <h2>Let's manage!</h2>
+    </div>
+  );
+}
+export default App;
