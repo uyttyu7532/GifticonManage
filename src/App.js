7c5f7bb17d280136791b0899785b6fa78d769d9d
uyttyu7532 <uyttyu7532@naver.com>
2020-08-21 02:56:16 +0900

    Material UI

--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
-import React from 'react';
+import React, { Component} from 'react';
 import './App.css';
 import Gifticon from './components/Gifticon';
+import Table from "@material-ui/core/Table";
+import Paper from "@material-ui/core/Paper";
+import TableHead from "@material-ui/core/TableHead";
+import TableBody from "@material-ui/core/TableBody";
+import TableRow from "@material-ui/core/TableRow";
+import TableCell from "@material-ui/core/TableCell";
+import {withStyles} from '@material-ui/core/styles';
+
+const styles = theme => ({
+  root: {
+    width: '100%',
+    marginTop: theme.spacing.unit * 1,
+    overflowX: "auto"
+  },
+  table:{
+    minWidth: 1080
+  }
+})
+
 
 const gifticons = [{
   'id' : 1,
@@ -24,24 +43,41 @@ const gifticons = [{
   'barcode_img' : 'https://placeimg.com/64/64/animals/3'
 }]
 
-function App() {
-  return (
-    <div>
-      {
-      gifticons.map(g => {
-        return(
-          <Gifticon
-          key = {g.id}
-          id = {g.id}
-          barcode_img = {g.barcode_img}
-          name = {g.name}
-          exp_date = {g.exp_date}
-          used = {g.used}
-        />
-        );
-      })
-    }
-    </div>
-  );
+class App extends Component {
+  render() {
+      const { classes } = this.props;
+      return (
+        <Paper className={classes.root}>
+          <Table className={classes.table}>
+            <TableHead>
+              <TableRow>
+                <TableCell>번호</TableCell>
+                <TableCell>기프티콘 사진</TableCell>
+                <TableCell>제목</TableCell>
+                <TableCell>유효기간</TableCell>
+                <TableCell>사용 여부</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+            {
+            gifticons.map(g => {
+              return(
+                <Gifticon
+                key = {g.id}
+                id = {g.id}
+                barcode_img = {g.barcode_img}
+                name = {g.name}
+                exp_date = {g.exp_date}
+                used = {g.used}
+              />
+              );
+          })
+        }
+          </TableBody>
+        </Table>
+      </Paper>
+    );
+  }
 }
-export default App;
+
+export default withStyles(styles)(App);
--- src/components/Gifticon.js
+++ src/components/Gifticon.js
@@ -1,37 +1,21 @@
 import React from "react";
+import TableRow from "@material-ui/core/TableRow";
+import TableCell from "@material-ui/core/TableCell";
 
 class Gifticon extends React.Component{
     render(){
         return(
-            <div>
-                <GifticonProfile id={this.props.id} barcode_img={this.props.barcode_img} name={this.props.name}/>           
-                <GifticonInfo exp_date={this.props.exp_date} used={this.props.used}/>
-            </div>
+            <TableRow>
+                <TableCell>{this.props.id}</TableCell>
+                <TableCell><img src={this.props.barcode_img} alt="barcode_img"></img></TableCell>
+                <TableCell>{this.props.name}</TableCell>
+                <TableCell>{this.props.exp_date}</TableCell>
+                <TableCell>{this.props.used}</TableCell>
+            </TableRow>
+        
         )
     }
 }
 
-class GifticonProfile extends React.Component{
-    render(){
-        return(
-            <div>
-                <h2>{this.props.id}</h2>
-                <h2>{this.props.name}</h2>
-                <img src={this.props.barcode_img} alt='barcode_img'/>
-            </div>
-        )
-    }
-}
-
-class GifticonInfo extends React.Component{
-    render(){
-        return(
-            <div>
-                <p>{this.props.exp_date}</p>
-                <p>{this.props.used}</p>
-            </div>
-        )
-    }
-}
 
 export default Gifticon;
