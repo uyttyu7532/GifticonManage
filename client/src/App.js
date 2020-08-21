5e9bca9702278dd88864cd710fc41ba049d5973b
uyttyu7532 <uyttyu7532@naver.com>
2020-08-21 23:29:20 +0900

    mysql연결

--- client/src/App.js
+++ client/src/App.js
@@ -8,35 +8,47 @@ import TableBody from "@material-ui/core/TableBody";
 import TableRow from "@material-ui/core/TableRow";
 import TableCell from "@material-ui/core/TableCell";
 import {withStyles} from '@material-ui/core/styles';
+import CircularProgress from '@material-ui/core/CircularProgress'
 
 const styles = theme => ({
   root: {
     width: '100%',
-    marginTop: theme.spacing.unit * 1,
+    marginTop: theme.spacing.unit * 3,
     overflowX: "auto"
   },
   table:{
     minWidth: 1080
+  },
+  progress:{
+    margin: theme.spacing.unit * 2
   }
 })
+
 
 
 class App extends Component {
 
   state = {
-    customers: ""
-  }
+    customers: "",
+    completed: 0
+  };
 
   componentDidMount() {
+    this.timer = setInterval(this.progress, 20);
     this.callApi()
-      .then(res => this.setState({gifticons: res}))
+      .then(res => this.setState({gifticons: res})) // 받아서 state로 설정 (서버에서 받은 res가  gifticons가 됨)
       .catch(err => console.log(err))
   }
 
   callApi = async () => {
-    const response = await fetch('/api/gifticons');
-    const body = await response.json();
+    const response = await fetch('/api/gifticons'); // 접속하고자 하는 api주소
+    const body = await response.json(); // 목록이 json 형태로 출력
     return body;
+  }
+
+  progress = () => {
+    const { completed } = this.state; // this.state.completed를 completed로 접근할 수 있게 해준다.
+    this.setState({completed: completed >= 100 ? 0 : completed + 1})
   }
 
   render() {
@@ -66,8 +78,13 @@ class App extends Component {
                 used = {g.used}
               />
               );
-          })
-        :""}
+          }):
+          <TableRow>
+            <TableCell colSpan="6" align="center">
+              <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed}/>
+            </TableCell>
+          </TableRow>
+        }
           </TableBody>
         </Table>
       </Paper>
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -6,30 +7,27 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}));
 
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host: conf.host,
+  user: conf.user,
+  password: conf.password,
+  port: conf.port,
+  database: conf.database
+});
+
+connection.connect();
+
 app.get('/api/gifticons', (req, res) => {
-    res.send(
-        [{
-            'id' : 1,
-            'name' : '스타벅스',
-            'exp_date' : '20200820',
-            'used' : 'false',
-            'barcode_img' : 'https://placeimg.com/64/64/animals/1'
-          },
-          {
-            'id' : 2,
-            'name' : '할리스',
-            'exp_date' : '20200819',
-            'used' : 'true',
-            'barcode_img' : 'https://placeimg.com/64/64/animals/2'
-          },
-          {
-            'id' : 3,
-            'name' : '커피빈',
-            'exp_date' : '20200818',
-            'used' : 'true',
-            'barcode_img' : 'https://placeimg.com/64/64/animals/3'
-          }]
-    );
+    connection.query(
+      "SELECT * FROM GIFTICON",
+      (err, rows, fields) => {
+        res.send(rows);
+      }
+    )
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
