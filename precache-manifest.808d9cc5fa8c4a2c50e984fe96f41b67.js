e528e772f803928d422040d7b5bf499a30e6349c
uyttyu7532 <uyttyu7532@naver.com>
2020-08-23 16:01:32 +0900

    Updates

---
+++ precache-manifest.808d9cc5fa8c4a2c50e984fe96f41b67.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d991e13560db72079837a9e70ed94e8",
+    "url": "/react_gifticon/index.html"
+  },
+  {
+    "revision": "462d464125c30761d242",
+    "url": "/react_gifticon/static/css/main.381b4614.chunk.css"
+  },
+  {
+    "revision": "bc25e9d6695317bbbd02",
+    "url": "/react_gifticon/static/js/2.68f24b4e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react_gifticon/static/js/2.68f24b4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "462d464125c30761d242",
+    "url": "/react_gifticon/static/js/main.f4418b65.chunk.js"
+  },
+  {
+    "revision": "9cb590a9e37384c1b441",
+    "url": "/react_gifticon/static/js/runtime-main.72b1ddd2.js"
+  }
+]);
