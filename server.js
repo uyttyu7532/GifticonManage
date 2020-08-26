fa5e158e30f5022ad90c28f244ed2fec97a8fc28
uyttyu7532 <uyttyu7532@naver.com>
2020-08-26 21:37:41 +0900

    cor font

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
+var cors = require('cors')();
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(cors());
+app.use(cors);
 
 const data = fs.readFileSync('./database.json');
 const conf = JSON.parse(data);
