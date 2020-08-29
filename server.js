3960f53e2e3b899bc862be62c650a4c123cb0f96
uyttyu7532 <uyttyu7532@naver.com>
2020-08-29 21:43:18 +0900

    ssl3

--- server.js
+++ server.js
@@ -38,8 +38,8 @@ app.get('/api/gifticons', (req, res) => {
 });
 
 app.get('/.well-known/pki-validation/08994B5BF518EB9466BE56F612849BF6.txt', (req, res) => {
-  res.send(`AED329E340B265007BA08E3DF576485AB9E9FEF4FAD3D96D09A6B53082CC76C6
-  comodoca.com
+  res.send(`AED329E340B265007BA08E3DF576485AB9E9FEF4FAD3D96D09A6B53082CC76C6<br>
+  comodoca.com<br>
   8d3eabd0094f156`);
 });
 
